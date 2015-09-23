4278b9cda63ad1effa08527b5488ef56da738bd4
Alin Posorovaschi <alin.posorovaschi@e-spres-oh.com>
2015-09-23 11:15:15 +0300

    Initial commit of the project generated using lein app template

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to tileserverstresstest
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/tileserverstresstest/core_test.clj
@@ -0,0 +1,7 @@
+(ns tileserverstresstest.core-test
+  (:require [clojure.test :refer :all]
+            [tileserverstresstest.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
