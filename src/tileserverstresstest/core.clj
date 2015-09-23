eafb94f803e7801f8eed18eb2443dcd5aa5329cd
Alin Posorovaschi <alin.posorovaschi@e-spres-oh.com>
2015-09-23 15:08:24 +0300

    Utility tile server stress testing functions

--- src/tileserverstresstest/core.clj
+++ src/tileserverstresstest/core.clj
@@ -1,7 +1,37 @@
 (ns tileserverstresstest.core
-  (:gen-class))
+  (:gen-class)
+  (:require
+    [clj-http.client :as client]
+    [clojure.data.json :as json]))
+
+(defn run-async
+  "Run the function on a separate thread of execution.
+  Exception are materialized and returned in the response.
+  Returns a future."
+  [fnct & args]
+  (future
+    (try
+      (Thread/sleep 200)
+      (let [start (. System (nanoTime))
+            res (apply fnct args)
+            end (. System (nanoTime))]
+        (assoc res :elapsed-time (/ (double (- (. System (nanoTime)) start)) 1000000.0)))
+      (catch Throwable e
+        {:status 500, :exception e}))))
+
+(defn run-times [n fnc]
+  (let [results (for [_ (range 0 n)] (fnc))]
+    (map (fn [f] @f) results)))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  "Application entry point"
+  [fct & args]
+  (apply (resolve (symbol fct)) args))
+
+
+(comment
+
+  (def res @(run-async client/get "http://localhost:4003/api/tms/12/4036/2564.png"))
+  (def res (map :elapsed-time (run-times 100 (partial run-async client/get "http://localhost:4003/api/tms/12/4036/2564.png"))))
+
+  )
