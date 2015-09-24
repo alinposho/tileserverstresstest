40cc1fce510e712c66e8edddb08c00bbafb4bff6
Alin Posorovaschi <alin.posorovaschi@e-spres-oh.com>
2015-09-24 16:55:30 +0300

    Function for drawing the request durations for a list of tile URLs

--- src/tileserverstresstest/core.clj
+++ src/tileserverstresstest/core.clj
@@ -39,18 +39,24 @@
     (run-async client/get (str base-endpoint lat "/" long ".png"))))
 
 (defn graph-for
-  ([{:keys [base-endpoint start-lat end-lat start-long end-long req-count]}]
-   (let [results (map (fn [f] @f)
-                      (flatten
-                        (repeatedly req-count (partial generate-tile base-endpoint start-lat end-lat start-long end-long))))
-         request-durations (map :elapsed-time (filter #(= 200 (:status %)) results))
-         request-start-times (map :request-start-time results)]
-     (view (line-chart request-start-times request-durations :title "Different tiles"))))
   ([endpoint req-count]
    (let [responses (filter #(= 200 (:status %)) (get-async-times endpoint req-count))
          elapsed-times (map :elapsed-time responses)
          request-time (map :request-start-time responses)]
-     (view (line-chart request-time elapsed-times :legend true :x-label "Request start time(ms)" :y-label "Request duration(ms)")))))
+     (view (line-chart request-time elapsed-times :legend true :x-label "Request start time(ms)" :y-label "Request duration(ms)"))))
+  ([{:keys [base-endpoint start-lat end-lat start-long end-long req-count]}]
+   (let [results (map (fn [f] @f)
+                      (flatten
+                        (repeatedly req-count (partial generate-tile base-endpoint start-lat end-lat start-long end-long))))
+         request-durations (map :elapsed-time results)
+         request-start-times (map :request-start-time results)]
+     (view (line-chart request-start-times request-durations :title "Different tiles")))))
+
+(defn graph-for-urls [urls]
+  (let [results (map (fn [f] @f) (map (partial run-async client/get) urls))
+        request-durations (map :elapsed-time results)
+        request-start-times (map :request-start-time results)]
+    (view (line-chart request-start-times request-durations :legend true :x-label "Request start time(ms)" :y-label "Request duration(ms)"))))
 
 (defn -main
   "Application entry point"
@@ -90,5 +96,18 @@
               :start-long    2564
               :end-long      2568
               :req-count     10})
+
+
+  ;; Call tileserver for a list of endpoints loaded from a file
+  #_(def urls (with-open [rdr (clojure.java.io/reader "resources/urls.txt")]
+                (line-seq rdr)))
+  (def urls (clojure.string/split-lines (slurp "resources/urls.txt")))
+
+  (def results (map (fn [f] @f) (map (partial run-async client/get) urls)))
+  (def request-durations (map :elapsed-time (filter #(= 200 (:status %)) results)))
+  (def request-start-times (map :request-start-time results))
+  (view (line-chart request-start-times request-durations :legend true :x-label "Request start time(ms)" :y-label "Request duration(ms)"))
+
+  (graph-for-urls urls)
 
   )
